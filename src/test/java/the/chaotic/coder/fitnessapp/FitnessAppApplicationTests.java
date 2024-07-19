85d52c35cd68abf3a8aa52c26310644bd4637851
YawHB <yawhboateng@gmail.com>
2024-07-19 12:42:31 +0200

    init commit

---
+++ src/test/java/the/chaotic/coder/fitnessapp/FitnessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package the.chaotic.coder.fitnessapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
