17b9c219e074cad137575e4fde47d36b992a29de
YawHB <yawhboateng@gmail.com>
2024-07-25 12:32:25 +0200

    Created custom exception handler for fitnessUser

---
+++ src/main/java/the/chaotic/coder/fitnessapp/exception/FitnessUserExistsException.java
@@ -0,0 +1,7 @@
+package the.chaotic.coder.fitnessapp.exception;
+
+public class FitnessUserExistsException extends RuntimeException {
+    public FitnessUserExistsException(String message) {
+        super(message);
+    }
+}
