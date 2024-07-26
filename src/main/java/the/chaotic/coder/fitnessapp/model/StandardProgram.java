aaf287d59c00a8a19e06e9a071eade761648bee5
YawHB <yawhboateng@gmail.com>
2024-07-26 15:51:14 +0200

    added service test class for standard-program

--- src/main/java/the/chaotic/coder/fitnessapp/model/StandardProgram.java
+++ src/main/java/the/chaotic/coder/fitnessapp/model/StandardProgram.java
@@ -1,10 +1,8 @@
 package the.chaotic.coder.fitnessapp.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
@@ -16,6 +14,7 @@ import the.chaotic.coder.fitnessapp.enums.FitnessCategory;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class StandardProgram {
 
     @Id
